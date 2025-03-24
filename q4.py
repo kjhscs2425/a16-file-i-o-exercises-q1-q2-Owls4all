5fca1c786732e662a0dd35efcf8d3c6ac263a4cc
owls4all <owls4all@gmail.com>
2025-03-24 09:25:17 -0700

    Q4 is confusing but I did it

--- q4.py
+++ q4.py
@@ -4,8 +4,17 @@ import json
 # Check if the file "data.json" exists.
 if os.path.isfile("data.json"):
     # If it does, read "data.json" into the variable `data`
+    data = {}
+    my_file = open("data.json","r").read()
+    for character in '{}"':
+        my_file = my_file.replace(character,'')
 
-    ####
+    for line in my_file.split(','):
+        pair = line.split(':')
+        if len(pair) == 2:
+            data[pair[0]]=pair[1]
+
+   
     #### YOUR CODE HERE 
     ####
 
@@ -21,6 +30,16 @@ recommendation = input("What book/movie/podcast/etc. would you recommend? ")
 data[name] = recommendation
 
 # Write the `data` variable to the file "data.json"
+my_file = open("data.json","w")
+first = True
+my_file.write('{')
+for key in data.keys():  
+    if first:
+        my_file.writelines(f'"{key}":"{data[key]}"')
+        first = False
+    else:
+        my_file.writelines(f',"{key}":"{data[key]}"')
+my_file.writelines('}')
 
 ####
 #### YOUR CODE HERE 
